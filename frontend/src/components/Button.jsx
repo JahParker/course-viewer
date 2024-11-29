0b0b48d2f064c5a7996ac846e85b31835bf820a5
JahParker <63827876+JahParker@users.noreply.github.com>
2024-11-29 12:04:40 -0500

    Create base for Button

--- frontend/src/components/Button.jsx
+++ frontend/src/components/Button.jsx
@@ -1,7 +1,53 @@
-const Button = () => {
+const Button = ({ variant }) => {
+  let color;
+  let action;
+  let content;
+
+  // May change onClick prop to be a function within Button
+  const addNew = () => {
+
+  }
+
+  const submit = () => {
+    
+  }
+
+  const cancel = () => {
+    
+  }
+
+  const deletion = () => {
+    
+  }
+
+  switch (variant) {
+    case "add":
+      color = "darkblue";
+      action = addNew;
+      content = "+"
+      break;
+    case "submit":
+      color = "darkblue";
+      action = submit;
+      content = "Submit"
+      break;
+    case "cancel":
+      color = "lightblue";
+      action = cancel;
+      content = "Cancel"
+      break;
+    case "delete":
+      color = "red";
+      action = deletion;
+      content = "Delete"
+      break;
+  }
+
   return (
-    <div>Button</div>
+    <button className={`button-${color}`} onClick={action}>
+      {content}
+    </button>
   )
 }
 
-export default Button
+export default Button;
