2b995d6f1c670597fd2032a16295233810d93d66
JahParker <63827876+JahParker@users.noreply.github.com>
2024-11-29 12:04:56 -0500

    Create base for Header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,7 +1,13 @@
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className="header">
+      <h1>Title</h1>
+      <div className="user-account">
+        <p>Welcome, ---</p>
+        <button>{"(Sign out)"}</button>
+      </div>
+    </div>
   )
 }
 
-export default Header
+export default Header;
